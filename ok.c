180685a71e4ba4e27115bb3cc6c0f07e60bc78cc
Marcio V <marcio203v1@gmail.com>
2025-11-24 20:58:10 -0300

    Create project overview for Viva Sabor
    Add project description for 'Viva Sabor' initiative.

---
+++ ok.c
@@ -0,0 +1,18 @@
+/*Hoje gostaria de compartilhar um projeto que eu e meu grupo idealizamos e desenvolvemos há algum tempo durante a matéria Empreendedorismo do programa Jovem Aprendiz IEL: Viva Sabor, uma iniciativa voltada para promover bem-estar na terceira idade por meio de receitas saudáveis e acompanhamento especializado.
+
+Esse projeto foi uma experiência extremamente enriquecedora, marcada por colaboração, troca de ideias e muita dedicação. Nosso objetivo sempre foi criar uma solução acessível, acolhedora e relevante para quem busca mais equilíbrio e qualidade de vida.
+
+Durante o desenvolvimento, estruturamos juntos o conceito, o conteúdo e a proposta do aplicativo e do site. Contribuí principalmente na parte visual e digital, criando telas e materiais que tornaram o processo mais intuitivo, funcional e criativo.
+
+Sou muito grato(a) aos meus colegas de equipe pela parceria, empenho e comprometimento durante cada etapa.
+
+Viva Sabor marcou de forma significativa minha trajetória acadêmica/profissional e segue sendo um lembrete do quanto podemos construir quando trabalhamos com propósito, sensibilidade e colaboração.
+#JovemAprendiz
+#JovemAprendizIELBA
+#Empreendedorismo
+#ProjetoAcadêmico
+#TrabalhoEmEquipe
+#TecnologiaEInovação
+#DesenvolvimentoDeAplicativos
+#Criatividade
+#Inovação*/
